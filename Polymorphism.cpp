ff675bd2f44620cb35bf2cb49f2477e3e1f7fe27
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2024-10-13 09:34:19 +0530

    Add files via upload

---
+++ Polymorphism.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  //Polymorphism -> one name many forms 
+  //eg.Method overloading -> method with same name but different parameters
+  //eg.Operator Overloading
+  //eg.Virtual Function
+   
+
+   //Polymorphism in c++ can be of two types
+  //  1.Compile time Polymorphism is acheived with
+  //  1.1-Function Overloading
+  //  1.2-Operator Overloading
+  //  2.Runtime time Polymorphism is acheived with
+  //  2.1-Virtual Functions
+  return 0;
+}
